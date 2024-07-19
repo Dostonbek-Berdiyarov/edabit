854a51826a368330ff413af458a78f19d3c54de6
Dostonbek-Berdiyarov <dostonbey.0726@gmail.com>
2024-07-19 17:46:39 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// function repetition(str, n) {
+//   return str.repeat(n);
+// }
+// console.log(repetition("Apple", 2));
+// console.log(repetition("Kiwi", 3));
+// console.log(repetition("Banana", 10));
+
+// function addition(n) {
+//   return n + 1;
+// }
+// console.log(addition(0));
+// console.log(addition(9));
+// console.log(addition(-3));
+
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// addition(3, 2);
+// addition(-3, -6);
+// addition(7, 3);
+
+// bu tekshirganda ishladi bu средний darajasi repeat the letters abc https://edabit.com/challenge/RftQjZs3srJnRBEH8
+
+// function removeABC(str) {
+//   if (str.match(/a/gi) || str.match(/b/gi) || str.match(/c/gi)) {
+//     return str.replace(/a/gi, "").replace(/b/gi, "").replace(/c/gi, "");
+//   } else {
+//     return null;
+//   }
+// }
+// console.log(removeABC("This might be a bit hard"));
+
+// removeABC("hello world!");
+
+// removeABC("");
+
+// bu tekshirganda ishladi bu средний darajasi repeat the letters abc https://edabit.com/challenge/RftQjZs3srJnRBEH8
+
+// function convert(minutes) {
+//   return minutes * 60;
+// }
+
+// function howManySeconds(hours) {
+//   return hours * 3600;
+// }
+
+// function calcAge(age) {
+//   return age * 365;
+// }
+
+// function getFirstValue(arr) {
+//   return arr[0];
+// }
+
+// function cubes(a) {
+// 	return a ** 3
+// }
+
+// function remainder(x, y) {
+//   return x % y;
+// }
+
+// function triArea(base, height) {
+//   return (base * height) / 2;
+// }
+
+// function nextEdge(side1, side2) {
+//   return side1 + side2 - 1;
+// }
+
+// function lessThanOrEqualToZero(num) {
+//   if (num <= 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
